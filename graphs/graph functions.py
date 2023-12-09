3a4dbe919cdbda8bc98086dc51938fb3408ad94f
Cameron Slaugh <cameronbyufan@gmail.com>
2023-12-08 23:07:16 -0700

    function

--- graphs/graph functions.py
+++ graphs/graph functions.py
@@ -1,1 +1,43 @@
-import pandas as pd
+def compare_weather_forecast(historical_df, forecast_df, date_column, value_column):
+    """
+    Compare historical weather data with the current forecast using a graph with a confidence interval.
+
+    Parameters:
+    - historical_df: DataFrame containing historical weather data.
+    - forecast_df: DataFrame containing the current weather forecast data.
+    - date_column: Name of the column in both DataFrames containing the date information.
+    - value_column: Name of the column in both DataFrames containing the weather variable to compare.
+
+    Returns:
+    - None (the function will display the graph).
+    """
+    # Merge the two DataFrames on the date_column
+    merged_df = pd.merge(historical_df, forecast_df, on=date_column, suffixes=('_historical', '_forecast'))
+
+    # Create a scatter plot with a line connecting the historical and forecast values
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(x=merged_df[date_column], y=merged_df[value_column + '_historical'], label='Historical')
+    sns.scatterplot(x=merged_df[date_column], y=merged_df[value_column + '_forecast'], label='Forecast')
+
+    # Add confidence interval around the forecast line
+    plt.fill_between(merged_df[date_column],
+                     merged_df[value_column + '_forecast'] - merged_df[value_column + '_forecast'].std(),
+                     merged_df[value_column + '_forecast'] + merged_df[value_column + '_forecast'].std(),
+                     alpha=0.2, color='blue')
+
+    # Set plot labels and title
+    plt.xlabel('Date')
+    plt.ylabel(value_column)
+    plt.title(f'Comparison of Historical Data and Forecast for {value_column}')
+
+    # Show legend
+    plt.legend()
+
+    # Show the plot
+    plt.show()
+
+# Example usage:
+# historical_data = pd.read_csv('historical_weather.csv')
+# forecast_data = pd.read_csv('forecast_weather.csv')
+# compare_weather_forecast(historical_data, forecast_data, 'date', 'temperature')
